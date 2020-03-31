2dcafbdcd5ff0f902ea0e2e2e4bcd7cd5834712a
T440_ubuntu <1203029076@qq.com>
2020-03-31 23:46:53 +0800

    丰富action_demo例子，增加反馈和返回结果的内容

--- action_demo/src/dishes_Server.cpp
+++ action_demo/src/dishes_Server.cpp
@@ -1,20 +1,43 @@
-   #include <action_demo/DoDishesAction.h>  // Note: "Action" is appended
-   #include <actionlib/server/simple_action_server.h>
+#include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include "action_demo/DoDishesAction.h"
 
-   typedef actionlib::SimpleActionServer<action_demo::DoDishesAction> Server;
+typedef actionlib::SimpleActionServer<action_demo::DoDishesAction> Server;
 
-   void execute(const action_demo::DoDishesGoalConstPtr& goal, Server* as)  // Note: "Action" is not appended to DoDishes here
-   {
-      // Do lots of awesome groundbreaking robot stuff here
-     as->setSucceeded();
-   }
+// execute when receive a goal from client
+void execute(const action_demo::DoDishesGoalConstPtr& goal, Server* as)
+{
+    ros::Rate rate(1);
+    action_demo::DoDishesFeedback feedback;
 
-  int main(int argc, char** argv)
-   {
-     ros::init(argc, argv, "dishes_Server");
-    ros::NodeHandle n;
-     Server server(n, "dishes", boost::bind(&execute, _1, &server), false);
+    ROS_INFO("Dishwasher %d is working.", goal->dishwasher_id);
+
+    // publish feedback
+    for(int i=1; i<=10; i++) {
+        feedback.percent_complete = i * 10;
+        as->publishFeedback(feedback);
+        rate.sleep();
+    }
+
+    // done. return result
+    ROS_INFO("Dishwasher %d finish working.", goal->dishwasher_id);
+    action_demo::DoDishesResult result;
+    result.total_dishes_cleaned = 10;
+    as->setSucceeded(result);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "do_dishes_server");
+    ros::NodeHandle nh;
+
+    // create a server
+    Server server(nh, "do_dishes", boost::bind(&execute, _1, &server), false);
+
+    // start the server
     server.start();
-     ros::spin();
+
+    ros::spin();
+
     return 0;
-   }
+}
